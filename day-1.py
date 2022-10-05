89ea9c72d559c323367a9b35b1ca9e1468a366f0
Zene09 <zeneorr@gmail.com>
2022-10-05 17:42:55 -0500

    nesting input within print command

--- day-1.py
+++ day-1.py
@@ -1,6 +1,9 @@
 # Band name generator program
 
 # practicing print command
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# nested commands
+print("Hello " + input("What is your name? "))
