73c79e76bf0e147e1b4b3f4b0d95555a35745521
Zene09 <zeneorr@gmail.com>
2022-10-16 13:57:45 -0500

    calculating length of a string

--- day-1.py
+++ day-1.py
@@ -6,4 +6,8 @@
 # print("print('what to print')")
 
 # nested commands
-print("Hello " + input("What is your name? "))
+# print("Hello " + input("What is your name? "))
+
+# Day 1 cont. 10/16/22
+# how to calculate the length of a string:
+print(len(input("What is your name? ")))
