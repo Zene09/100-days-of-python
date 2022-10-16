b0e58117836c8e432fca3263355ffeb297812e32
Zene09 <zeneorr@gmail.com>
2022-10-16 14:03:22 -0500

    playing with Python variables

--- day-1.py
+++ day-1.py
@@ -10,4 +10,12 @@
 
 # Day 1 cont. 10/16/22
 # how to calculate the length of a string:
-print(len(input("What is your name? ")))
+# print(len(input("What is your name? ")))
+
+# python variables - a cascading language
+# this saves the data for later
+name = input("What is your name? ")
+length = len(name)
+
+print(name)
+print(length)
